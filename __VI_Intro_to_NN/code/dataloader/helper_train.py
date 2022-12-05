a122e0d4441224d1d80fc877e38821efd34c3a04
v1set <lukasusonis@gmail.com>
2022-12-05 17:56:06 +0200

    2022/12/5 namu_darbai_#3

---
+++ __VI_Intro_to_NN/code/dataloader/helper_train.py
@@ -0,0 +1,62 @@
+import time
+import torch
+from helper_evaluation import compute_accuracy
+
+
+def train_model(model, 
+                num_epochs, 
+                train_loader,
+                valid_loader, 
+                test_loader, 
+                optimizer, 
+                device):
+
+    start_time = time.time()
+    minibatch_loss_list, train_acc_list, valid_acc_list = [], [], []
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, targets) in enumerate(train_loader):
+
+            features = features.to(device)
+            targets  = targets.to(device)
+
+            ### skaičiavimas pirmyn ir atgal
+            logits = model(features)
+            loss   = torch.nn.functional.cross_entropy(logits, targets)
+            optimizer.zero_grad()
+            loss.backward()
+
+            ### modelio parametrų atnaujinimas
+            optimizer.step()
+
+            ### registras
+            minibatch_loss_list.append(loss.item())
+            if not batch_idx % 50:
+                
+                print(f'  Epoch: {epoch+1:03d}/{num_epochs:03d} '
+                      f'| Batch {batch_idx:04d}/{len(train_loader):04d} '
+                      f'| Loss: {loss:.4f}')
+
+        model.eval()
+        with torch.no_grad():  # sutaupo atminties darant išvadas/prognozes
+            train_acc = compute_accuracy(model, train_loader, device=device)
+            valid_acc = compute_accuracy(model, valid_loader, device=device)
+            
+            print(f'  Epoch: {epoch+1:03d}/{num_epochs:03d} '
+                  f'| Train: {train_acc :.2f}% '
+                  f'| Validation: {valid_acc :.2f}%')
+            
+            train_acc_list.append(train_acc.item())
+            valid_acc_list.append(valid_acc.item())
+
+        elapsed = (time.time() - start_time)/60
+        print(f'Time elapsed: {elapsed:.2f} min')
+
+    elapsed = (time.time() - start_time)/60
+    print(f'Total Training Time: {elapsed:.2f} min')
+
+    test_acc = compute_accuracy(model, test_loader, device=device)
+    print(f'Test accuracy {test_acc :.2f}%')
+
+    return minibatch_loss_list, train_acc_list, valid_acc_list
